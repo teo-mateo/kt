5eddac855f43348b0e8c079a082e0be525f6eae7
Teodor B <tbardici@gmail.com>
2015-05-23 23:50:37 +0200

    added knockoutjs

---
+++ kt.client/Scripts/kt.js
@@ -0,0 +1,12 @@
+﻿var x = 12;
+
+var api_root = "http://localhost:63611/";
+var api_decks = api_root + "api/Decks";
+
+$.getJSON(api_decks, function (data) {
+
+    var c = data;
+
+    // Now use this data to update your view models, 
+    // and Knockout will update your UI automatically 
+})
